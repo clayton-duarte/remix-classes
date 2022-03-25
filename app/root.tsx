c73271af3c4e5e48bed425f1c9639790ffb1aaac
clayton <clayton@null.net>
2022-03-25 14:30:01 -0400

    :zap: Updates root layout

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,5 @@
+import { ReactNode } from "react";
+
 import { Theme, ThemeProvider, Global, css } from "@emotion/react";
 import styled from "@emotion/styled";
 import {
@@ -46,10 +48,6 @@ const ContentLayout = styled.article`
   gap: 1rem;
 `;
 
-const FakeLogo = styled.h1`
-  font-size: 1.25rem;
-`;
-
 const theme: Theme = {
   primary: "#17405F",
   secondary: "#6B6C6D",
@@ -60,6 +58,96 @@ const theme: Theme = {
   white: "#FEFEFE",
   bg: "#DCDBCC",
 };
+
+function Layout({ children }: { children: ReactNode }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <RootLayout>
+        <HeaderLayout>
+          <Link to="/">
+            <summary>{`D&D4 Tools`}</summary>
+          </Link>
+        </HeaderLayout>
+        <ToasterProvider>
+          <ContentLayout>{children}</ContentLayout>
+        </ToasterProvider>
+      </RootLayout>
+      <Global
+        styles={(theme) => css`
+          html,
+          body {
+            background: ${theme.white};
+            font-family: "Spectral", serif;
+            color: ${theme.black};
+            line-height: 1.2;
+            font-weight: 400;
+            font-size: 16px;
+            margin: 0;
+          }
+
+          button {
+            font-family: "Cinzel", serif;
+            font-weight: 700;
+            line-height: 1.2;
+            font-size: 1rem;
+          }
+
+          a:visited,
+          a:active,
+          a:hover,
+          a:focus,
+          a {
+            text-decoration: underline;
+            color: inherit;
+          }
+
+          [role="button"],
+          button,
+          a {
+            cursor: pointer;
+          }
+
+          p,
+          h1,
+          h2,
+          h3,
+          h4,
+          h5,
+          h6,
+          legend,
+          summary {
+            margin: 0;
+            &:first-letter {
+              text-transform: capitalize;
+            }
+          }
+
+          h1,
+          h2,
+          h3,
+          h4,
+          h5,
+          h6,
+          legend,
+          summary {
+            font-family: "Cinzel", serif;
+            font-weight: 700;
+            line-height: 1;
+          }
+
+          ul,
+          ol {
+            padding: 0 0 0 1rem;
+            margin: 0;
+            li {
+              list-style: none;
+            }
+          }
+        `}
+      />
+    </ThemeProvider>
+  );
+}
 
 // TODO
 export function ErrorBoundary({ error }: { error: Error }) {
@@ -73,8 +161,12 @@ export function ErrorBoundary({ error }: { error: Error }) {
         <Links />
       </head>
       <body>
-        {/* add the UI you want your users to see */}
-        <Scripts />
+        <Layout>
+          <h1>Oh no!</h1>
+          {/* add the UI you want your users to see */}
+          <Link to="/">Go home</Link>
+          <Scripts />
+        </Layout>
       </body>
     </html>
   );
@@ -91,10 +183,12 @@ export function CatchBoundary() {
         <Links />
       </head>
       <body>
-        <h1>
-          {caught.status} {caught.statusText}
-        </h1>
-        <Link to="/">Go home</Link>
+        <Layout>
+          <h1>
+            {caught.status} {caught.statusText}
+          </h1>
+          <Link to="/">Go home</Link>
+        </Layout>
         <Scripts />
       </body>
     </html>
@@ -128,88 +222,9 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <ThemeProvider theme={theme}>
-          <RootLayout>
-            <HeaderLayout>
-              <Link to="/">
-                <FakeLogo>D&D4 Tools</FakeLogo>
-              </Link>
-            </HeaderLayout>
-            <ToasterProvider>
-              <ContentLayout>
-                <Outlet />
-              </ContentLayout>
-            </ToasterProvider>
-          </RootLayout>
-          <Global
-            styles={(theme) => css`
-              html,
-              body {
-                background: ${theme.white};
-                font-family: "Spectral", serif;
-                color: ${theme.black};
-                line-height: 1.2;
-                font-weight: 400;
-                font-size: 16px;
-                margin: 0;
-              }
-
-              button {
-                font-family: "Cinzel", serif;
-                font-weight: 700;
-                line-height: 1.2;
-                font-size: 1rem;
-              }
-
-              a:visited,
-              a:active,
-              a:hover,
-              a:focus,
-              a {
-                text-decoration: underline;
-                color: inherit;
-              }
-
-              [role="button"],
-              button,
-              a {
-                cursor: pointer;
-              }
-
-              p {
-                margin: 0;
-              }
-
-              p,
-              h1,
-              h2,
-              h3,
-              h4,
-              h5,
-              h6,
-              legend,
-              summary {
-                &:first-letter {
-                  text-transform: capitalize;
-                }
-              }
-
-              h1,
-              h2,
-              h3,
-              h4,
-              h5,
-              h6,
-              legend,
-              summary {
-                font-family: "Cinzel", serif;
-                font-weight: 700;
-                line-height: 1;
-                margin: 0;
-              }
-            `}
-          />
-        </ThemeProvider>
+        <Layout>
+          <Outlet />
+        </Layout>
         <LiveReload />
         <Scripts />
       </body>
